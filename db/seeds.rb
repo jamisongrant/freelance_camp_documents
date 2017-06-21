2ffdf065cf9d03036f6b0c14ebb60832f03dd0e9
jamisongrant <jgbatchelor@gmail.com>
2017-06-20 18:51:34 -0600

    updating old files to run on new versions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 # create_table "freelance_documents", force: :cascade do |t|
-#   t.string   "title"
-#   t.string   "description"
-#   t.text     "file_url"
-#   t.text     "image_url"
-#   t.datetime "created_at",  null: false
-#   t.datetime "updated_at",  null: false
-# end
-
+#     t.string   "title"
+#     t.string   "description"
+#     t.text     "file_url"
+#     t.text     "image_url"
+#     t.datetime "created_at",  null: false
+#     t.datetime "updated_at",  null: false
+#   end
 
 10.times do |d|
-FreelanceDocument.create!(
-  title: "Document #{d}",
-  description: "this is a document this is only a document and as a document this is a test like yours so don't judge me at all",
-  file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing"
-)
+  FreelanceDocument.create!(
+    title: "Document #{d}",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+    file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
+    image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
+  )
 end
